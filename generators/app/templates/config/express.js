af2dc60042db1760733168ad93bc1e06f882e145
tarungarg546 <tarungarg546@gmail.com>
2016-06-19 02:29:13 +0530

    Path fixed

--- generators/app/templates/config/express.js
+++ generators/app/templates/config/express.js
@@ -2,11 +2,11 @@ module.exports = function(express) {
   'use strict';
 	let express = express || require('express');
 	const app = express();
-	app.set('views', path.join(__dirname, '/../views'));
+	app.set('views', __dirname+'/../views');
 	app.set('view engine', 'ejs');
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded());
 	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, '/../public')));
+	app.use(express.static(__dirname+'/../public'));
 	app.set('port', process.env.PORT || 8080);
 }
